fd41b3e107dfca078657b366351f026bb86c2085
Joseph Vijay J <87023714+josephvijayJ@users.noreply.github.com>
2022-04-07 15:36:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const server = app.listen(
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://shiny-creponne-175251.netlify.app/',
     // credentials: true,
   },
 });
